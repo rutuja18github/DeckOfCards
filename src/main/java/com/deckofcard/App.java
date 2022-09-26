59f97da256c3066225e1159e9c503ac0183d6871
RutujaPatil18 <prutuja189@gmail.com>
2022-09-26 08:40:32 +0530

    ADD:First commit

---
+++ src/main/java/com/deckofcard/App.java
@@ -0,0 +1,11 @@
+package com.deckofcard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Welcome To Deck Of Cards Game");
+	}
+}
